92cce36ce51df1b03b3956bdd6b05a8da91c2fc2
jorge-73 <jorge_ap_73@hotmail.com>
2024-09-11 00:38:30 -0300

    ejemplo

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <button className="btn btn-error">Button</button>
+    </div>
+  );
+}
+
+export default App;
